8648bbd7aff4a56dba7781b3f0211d81e931cd61
zeon77 <zeon77@gmail.com>
2021-06-04 12:33:19 +0200

    2-3. feladat

--- Rostok/Program.cs
+++ Rostok/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Rostok
 {
@@ -10,6 +10,12 @@ namespace Rostok
     {
         static void Main(string[] args)
         {
+            //2.
+            List<Rost> rostok = new List<Rost>();
+            foreach (var sor in File.ReadAllLines("rostok.txt").Skip(1)) rostok.Add(new Rost(sor));
+
+            //3.
+            Console.WriteLine($"3. feladat: Élelmiszerek száma: {rostok.Count}");
 
             Console.ReadKey();
         }
